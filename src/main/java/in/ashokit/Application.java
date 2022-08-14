a59f9bda77dafcfb38d50a1a874b6d8a590c8545
Niketan <nickthombare@gmail.com>
2022-08-14 17:22:07 +0530

    added

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+         int a=10;
+         int b=20;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-System.out.println();
-System.out.println("priority task done");
+
+             System.out.println();
+             System.out.println("priority task done");
+             System.out.println("addition of and b is :"+(a+b));
+              System.out.println("start class ended...!");
+
 	}
 
 
